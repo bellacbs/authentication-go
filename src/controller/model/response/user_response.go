f53bbf12f4510f68363df1e447148b943398ab8c
Izabella <bellacbrandao@gmail.com>
2023-12-12 18:27:54 -0300

    logggers

--- src/controller/model/response/user_response.go
+++ src/controller/model/response/user_response.go
@@ -1,8 +1,7 @@
 package response
 
 type UserResponse struct {
-	ID       string `json:"id"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-	Name     string `json:"name"`
+	ID    string `json:"id"`
+	Email string `json:"email"`
+	Name  string `json:"name"`
 }
--- src/controller/user/createUser.go
+++ src/controller/user/createUser.go
@@ -3,20 +3,35 @@ package userController
 import (
 	"net/http"
 
+	"github.com/bellacbs/authentication-go/src/configuration/logger"
 	"github.com/bellacbs/authentication-go/src/configuration/validation"
 	"github.com/bellacbs/authentication-go/src/controller/model/request"
+	"github.com/bellacbs/authentication-go/src/controller/model/response"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"go.uber.org/zap"
 )
 
 func CreateUser(c *gin.Context) {
+	logger.Info("Init CreateUser controller", zap.String("journey", "createUser"))
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		logger.Error("Error trying to validate user info", err,
+			zap.String("journey", "createUser"))
 		restError := validation.ValidateUserError(err)
 
 		c.JSON(restError.Code, restError)
 	}
 
-	c.JSON(http.StatusOK, userRequest)
+	response := response.UserResponse{
+		ID:    uuid.New().String(),
+		Email: userRequest.Email,
+		Name:  userRequest.Name,
+	}
+
+	logger.Info("User created succefully", zap.String("journey", "createUser"))
+
+	c.JSON(http.StatusOK, response)
 
 }
